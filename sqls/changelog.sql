6a5712d417ad33264a7b334430214abb67100174
zhouxuxian <zhouxuxian@meituan.com>
2024-07-30 16:21:35 +0800

    "添加自动生成插件，修改数据库配置"

--- sqls/changelog.sql
+++ sqls/changelog.sql
@@ -1,14 +1,14 @@
 use paotuitui;
-create table user(
+create table student(
                      id bigint primary key auto_increment,
                      student_id bigint not null comment '学号',
                      phone_number varchar(20) comment '手机号',
                      auth_state tinyint default 1 comment '0 未认证 1 已认证'
 );
 create table audit(
-                      id int primary key auto_increment,
+                      id bigint primary key auto_increment,
                       student_id bigint not null comment '学号',
                       status tinyint not null comment '审核状态 0 待审核 1 通过，2 未通过',
-                      img_str varchar(10000) not null comment '图片base64编码后的值'
+                      img_str varchar(10000) not null comment '图片地址'
 );
 
